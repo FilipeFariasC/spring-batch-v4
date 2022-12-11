3502bb4deb82f3b542f6563de06580428a969c9c
Filipe Farias <filipe.chagas@klok.tech>
2022-12-11 16:09:07 -0300

    inicializacao do projeto

---
+++ src/main/java/br/edu/ifpb/projetoum/springbatch/SpringBatchV4UpcensusTestApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.projetoum.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchV4UpcensusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchV4UpcensusTestApplication.class, args);
+	}
+
+}
