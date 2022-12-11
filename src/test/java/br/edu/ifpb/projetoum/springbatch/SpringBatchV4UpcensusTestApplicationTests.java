aa7990c25fda15b0ec8b8b69f8d2d6686b488ea3
Filipe Farias <filipe.farias.chagas@hotmail.com>
2022-12-11 16:09:07 -0300

    inicializacao do projeto

---
+++ src/test/java/br/edu/ifpb/projetoum/springbatch/SpringBatchV4UpcensusTestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.projetoum.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchV4UpcensusTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
